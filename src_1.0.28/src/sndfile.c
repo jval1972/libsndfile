048d3fa3b7f0c37ccac5c3a0e121484559b95d4e
jval1972 <jvalavanis@gmail.com>
2021-01-14 15:24:09 +0200

    mytolower() func

--- src_1.0.28/src/sndfile.c
+++ src_1.0.28/src/sndfile.c
@@ -2597,6 +2597,39 @@ sf_writef_double	(SNDFILE *sndfile, const double *ptr, sf_count_t frames)
 ** Private functions.
 */
 
+int mytolower(char c) {
+        switch(c)       {
+        case 'A': return 'a';
+        case 'B': return 'b';
+        case 'C': return 'c';
+        case 'D': return 'd';
+        case 'E': return 'e';
+        case 'F': return 'f';
+        case 'G': return 'g';
+        case 'H': return 'h';
+        case 'I': return 'i';
+        case 'J': return 'j';
+        case 'K': return 'k';
+        case 'L': return 'l';
+        case 'M': return 'm';
+        case 'N': return 'n';
+        case 'O': return 'o';
+        case 'P': return 'p';
+        case 'Q': return 'q';
+        case 'R': return 'r';
+        case 'S': return 's';
+        case 'T': return 't';
+        case 'U': return 'u';
+        case 'V': return 'v';
+        case 'W': return 'w';
+        case 'X': return 'x';
+        case 'Y': return 'y';
+        case 'Z': return 'z';
+        default:
+                return c;
+        }
+}
+
 static int
 try_resource_fork (SF_PRIVATE * psf)
 {	int old_error = psf->error ;
@@ -2633,7 +2666,7 @@ format_from_extension (SF_PRIVATE *psf)
 	/* Convert everything in the buffer to lower case. */
 	cptr = buffer ;
 	while (*cptr)
-	{	*cptr = tolower (*cptr) ;
+	{	*cptr = mytolower (*cptr) ;
 		cptr ++ ;
 		} ;
 
